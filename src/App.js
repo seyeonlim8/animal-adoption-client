9ae0d5762f8f45c891412ade6dd5bcaf375ce790
Seyeon Lim <seyeonlim8@gmail.com>
2024-08-22 14:37:56 +0900

    added css and a home page js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Animals from "./routes/Animals";
 import Animal from "./routes/Animal";
+import Home from "./routes/Home";
 
 function App() {
   return <BrowserRouter>
   <Routes>
-    <Route path="/" element={<Animals />} />
+    <Route path="/" element={<Home />} />
+    <Route path="/animal" element={<Animals />} />
     <Route path="/animal/:id" element={<Animal />} />
   </Routes>
   </BrowserRouter>;
